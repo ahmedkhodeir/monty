fe4dee3a2f0bcadbb9c8057d7ffca43ac4ef5bd3
Ahmed Khodeir <110243896+ahmedkhodeir@users.noreply.github.com>
2023-08-27 01:27:52 +0300

    update

--- monty.h
+++ monty.h
@@ -9,13 +9,11 @@
 #include <ctype.h>
 
 /**
-*struct stack_s - a program that doubly linked list
-*representation of a stack (or queue)
+*struct stack_s -  program that doubly
 *@n: integer
-*@prev: points to the previous element of the stack (or queue)
-*@next: points to the next element of the stack (or queue)
-*Description: doubly linked list node structure
-*for stack, queues, LIFO, FIFO Holberton project
+*@prev: points to the previous element.
+*@next: points to the next element.
+*Description: doubly linked list.
 */
 typedef struct stack_s
 {
