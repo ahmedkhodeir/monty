89c5b26755cde13208e684d33f2df70073e08017
Ahmed Khodeir <110243896+ahmedkhodeir@users.noreply.github.com>
2023-08-27 01:28:37 +0300

    update

--- nop.c
+++ nop.c
@@ -1,10 +1,10 @@
 #include "monty.h"
 
 /**
-*f_nop- nothing
-*@head: stack head
-*@counter: line_number
-*Return: nothing!
+*f_nop- nothing.
+*@head: stack head.
+*@counter: line_number.
+*Return: nothing.
 */
 void f_nop(stack_t **head, unsigned int counter)
 {
